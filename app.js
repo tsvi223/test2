649e100b5640970f2f645990a98118e5d3883eb3
tsvi223 <tsvi223@gmail.com>
2016-03-16 20:28:56 +0000

    lk

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-/*/var express = require("express");
+var express = require("express");
 var host = process.env.LISTEN_IP || "0.0.0.0";
-var port = process.env.LISTEN_PORT || 6060;
+var port = process.env.LISTEN_PORT || 8080;
 
 var app = express();
 //var y = 0;
@@ -19,7 +19,7 @@ console.log('work');
 app.listen(port, host, function(){
     console.log('Starting PWeb Server on Port   ' + port );
 });
-*/
+
 var http = require("http");
 var express = require("express");
 var app = express();
@@ -27,9 +27,10 @@ app.get('/',function(req, res){
   
 res.send('kkk');  
 })
-/*
+
 // create a server
 http.createServer(function(req, res) {
     // on every request, we'll output 'Hello world'
     res.end("Hello world from Cloud9!");
-}).listen(process.env.PORT, process.env.IP);*/
+}).listen(process.env.PORT, process.env.IP);
+
